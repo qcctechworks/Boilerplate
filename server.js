b95c476cc584fd962de7407f4664661ac98cbc4f
CaptainKRS <qcctechworks@gmail.com>
2020-07-20 14:54:06 -0500

    Added all the changes

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const app = express();
 const morgan = require('morgan');
 const port = 4000;
 
+
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/api/students', (req,res) => {
+    const students = [
+        { id: 1, firstName: 'Captain', lastName: "Fancy"},
+        { id: 2, firstName: 'John', lastName: 'Buttercup'}
+    ]
+    res.json(students);
+});
+
 app.listen(port, () => {
     console.log(`Server Started on Port # ${port}`);
-})
+});
+
 
